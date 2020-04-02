73bfb7954d439f6f334271a565532438b7c85885
gonearewe <cristmactavish@outlook.com>
2020-04-02 20:57:59 +0800

    preparing for tests

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 #![no_std]
 #![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
 
@@ -8,7 +11,11 @@ mod vga_buffer;
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
     println!("Hello World !!!");
-    panic!("test panic");
+
+    #[cfg(test)]
+        test_main();
+
+    loop {}
 }
 
 // we must implement our own panic!
@@ -16,4 +23,26 @@ pub extern "C" fn _start() -> ! {
 fn panic(_info: &PanicInfo) -> ! {
     println!("{}", _info);
     loop {}
+}
+
+#[test_case]
+fn trivial_test() {
+    print!("trivial assertion ... ");
+    assert_eq!(1, 1);
+    println!("[OK]")
+}
+
+#[cfg(test)]
+fn test_runner(tests: &[&dyn Fn()]) {
+    println!("Running {} tests", tests.len());
+    for test in tests {
+        test();
+    }
+}
+
+#[derive(Copy, Clone, Eq, PartialEq, Debug)]
+#[repr(u32)]
+pub enum QemuExitCode {
+    Success = 0x10,
+    Failure = 0x11,
 }
